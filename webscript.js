cbc12d8e5ae3bfbe562d079add3d4abd8829b35d
Lonent <98558264+Lonent@users.noreply.github.com>
2022-11-29 13:05:00 +0300

    Update webscript.js

--- webscript.js
+++ webscript.js
@@ -1,5 +1,5 @@
 let changebutton = document.getElementById("ClrBtn");
 
 function ChangeColor() {
-  document.body.style.backgroundColor = "green";
+  document.body.style.backgroundColor = color;
 }
